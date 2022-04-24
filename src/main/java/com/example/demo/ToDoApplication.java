8776a16db9cbf7bf03ee2bf1d4fcac5e521c7164
Yussuf Olanrewaju <larrybullet0110@gmail.com>
2022-04-24 16:52:21 +0100

    first commit

---
+++ src/main/java/com/example/demo/ToDoApplication.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import com.example.demo.entity.Todo;
+import com.example.demo.entity.User;
+import com.example.demo.repository.TodoRepository;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private TodoRepository todoRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		User user = new User();
+		user.setId(1L);
+		user.setPassword("should be hashed");
+		user.setUsername("Adam");
+
+		Todo todo = new Todo();
+		todo.setId(1L);
+		todo.setContent("Ramadan");
+
+		user.getTodoList().add(todo);
+
+		todoRepository.save(todo);
+		userRepository.save(user);
+	}
+}
